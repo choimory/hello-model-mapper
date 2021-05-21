4293f99b7e8ea532d7b6faf624edc81cc618b86b
choimory <choimory@naver.com>
2021-05-21 18:50:24 +0900

    Initial commit

---
+++ src/main/java/com/practice/hellomodelmapper/HellomodelmapperApplication.java
@@ -0,0 +1,13 @@
+package com.practice.hellomodelmapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellomodelmapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellomodelmapperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/hellomodelmapper/HellomodelmapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.hellomodelmapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellomodelmapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
